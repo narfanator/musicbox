44bd21251b6750db1d6d3281b05a1a08db92b8e6
Nick Baone <narfanator@gmail.com>
2016-12-15 14:22:44 -0800

    Basic Input

---
+++ Assets/Scripts/VRInput.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+public class VRInput : MonoBehaviour {
+
+    private SteamVR_Controller.Device controller;
+    private SteamVR_TrackedObject trackedObj;
+
+    void Start() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        controller = SteamVR_Controller.Input((int)trackedObj.index);
+    }
+
+    void Update() {
+        if (controller == null) {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Collider enter: " + other.name);
+        if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Enter press down");
+            //joint = other.gameObject.AddComponent<FixedJoint>();
+            //joint.connectedBody = this.GetComponent<Rigidbody>();
+        }
+        if (controller.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Enter press up");
+            //Destroy(joint);
+        }
+    }
+
+    //private FixedJoint joint;
+    void OnTriggerStay(Collider other) {
+        //Debug.Log("Stay: " + other.name);
+        if(controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Stay press down");
+            //joint = other.gameObject.AddComponent<FixedJoint>();
+            //joint.connectedBody = this.GetComponent<Rigidbody>();
+        }
+        if (controller.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Stay press up");
+            //Destroy(joint);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("Collider exit: " + other.name);
+        if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Exit press down");
+            //joint = other.gameObject.AddComponent<FixedJoint>();
+            //joint.connectedBody = this.GetComponent<Rigidbody>();
+        }
+        if (controller.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Exit press up");
+            //Destroy(joint);
+        }
+    }
+
+}
